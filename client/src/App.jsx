1e93248e3be46b3ad4f968df60aff3bfa2b0e035
itssadiq <sadiqalishaikh009@gmail.com>
2025-11-29 00:29:35 +0500

    - Setup backend and created API to send todos data to frontend.
    - Fetched API to frontend succesfully.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,26 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import InputPage from "./pages/InputPage";
 import WelcomePage from "./pages/WelcomePage";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    async function fetchTodos() {
+      // TODO: Add error handling here.
+      const todos = await fetch("http://localhost:3000/todos");
+
+      const data = await todos.json();
+
+      setTodos(data);
+    }
+
+    fetchTodos();
+  }, []);
+
+  console.log(todos);
+
   return (
     <>
       <Routes>
--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const todos = require("./data/todos");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 const PORT = 3000;
+
+app.use("/todos", (req, res) => {
+  res.send(todos);
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on Port", PORT);
