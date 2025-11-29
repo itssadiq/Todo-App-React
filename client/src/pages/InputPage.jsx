1c69daf1c520c96c048109fcc792711620fae60b
itssadiq <sadiqalishaikh009@gmail.com>
2025-11-29 17:39:37 +0500

    Created new API for adding todo and rendered on page.

--- client/src/pages/InputPage.jsx
+++ client/src/pages/InputPage.jsx
@@ -1,23 +1,61 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const InputPage = () => {
+  const [task, setTask] = useState("");
+  const [date, setDate] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleTaskInput = (e) => {
+    const value = e.target.value;
+
+    setTask(value);
+  };
+
+  const handleDateInput = (e) => {
+    const value = e.target.value;
+
+    setDate(value);
+  };
+
+  const handleCategoryInput = (e) => {
+    const value = e.target.value;
+
+    setCategory(value);
+  };
+
+  const addToDo = () => {
+    console.log(task);
+    console.log(date);
+    console.log(category);
+  };
+
   return (
     <div className="main-03">
       <input
         type="text"
         className="task-input"
         placeholder="Write a task..."
+        onChange={handleTaskInput}
         required
       />
-      <input type="date" className="task-date" required />
+      <input
+        type="date"
+        className="task-date"
+        onChange={handleDateInput}
+        required
+      />
       <input
         type="text"
         className="task-category"
         placeholder="Write Category..."
+        onChange={handleCategoryInput}
         required
       />
       <div className="btn-container">
-        <button className="add-button">Add</button>
+        <button className="add-button" onClick={addToDo}>
+          Add
+        </button>
         <Link to="/home">
           <button className="back-button">Back</button>
         </Link>
--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const todos = require("./data/todos");
 const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
+
 app.use(cors());
+
+app.use(express.json());
 
 const PORT = 3000;
 
-app.use("/todos", (req, res) => {
+app.get("/todos", (req, res) => {
   res.send(todos);
+});
+
+app.post("/todos", (req, res) => {
+  const body = req.body;
+
+  todos.push({ ...body, id: todos.length + 1, completed: false });
+
+  const filePath = path.join(__dirname, "data", "todos.json");
+
+  fs.writeFile(filePath, JSON.stringify(todos), (err, data) => {
+    return res.json({ message: "Todo Added" });
+  });
 });
 
 app.listen(PORT, () => {
